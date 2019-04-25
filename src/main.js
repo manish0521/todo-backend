0f9c3c81ad39f885ffd7715d4b707e26ad65a0d3
manish singh <manish.singh@codeimmersives.com>
2019-04-25 14:20:50 -0400

    post added

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 window.onload = init;
 
+
+
 function init() {
-    document.querySelector('#get').addEventListener('click', getTodos);
+    getTodos()
+    // document.querySelector('#get').addEventListener('click', getTodos);
     document.querySelector('#post').addEventListener('click', postTodo);
     document.querySelector('#put').addEventListener('click', updateThirdTodo);
 }
 
 function getTodos(event) {
-    event.preventDefault();
+    // event.preventDefault();
 
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'http://localhost:3000/todos');
@@ -18,20 +21,30 @@ function getTodos(event) {
 
 function postTodo(event) {
     event.preventDefault();
+    // alert ("alert!");
+    const userInput = document.querySelector('#user-input').value;
+    if(userInput !== "") {
+        const newTodo = {
+            text: userInput,
+            completed: false
+        };
+        const jsonnedTodo = JSON.stringify(newTodo);
+        
+        
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://localhost:3000/todos');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onload = getTodos;
+        xhr.send(jsonnedTodo);
+        
+        document.querySelector('#user-input').value = '';
 
-    const newTodo = {
-        text: "Profit",
-        completed: false
-    };
-    
-    const jsonnedTodo = JSON.stringify(newTodo);
-    
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:3000/todos');
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onload = handleData;
-    xhr.send(jsonnedTodo);
+    }
 }
+
+
+
+
 
 function updateThirdTodo(event) {
     const updatedTodo = {
@@ -48,5 +61,28 @@ function updateThirdTodo(event) {
 }
 
 function handleData(event) {
-    console.log(event.target.responseText);
+   const todoData = JSON.parse(event.target.responseText);
+//    call clear funtion here
+ clear();
+
+   for(i = 0; i < todoData.length; i++){
+    showMe = todoData[i].text;
+    
+    
+    const newLi = document.createElement('li');
+    newLi.innerText = showMe;
+
+
+    const newlist = document.querySelector('#todo-list');
+    newlist.appendChild(newLi);
+
+   }
 }
+
+function clear(){
+    const newlist = document.querySelector('#todo-list');
+
+    while(newlist.hasChildNodes()) {
+        newlist.removeChild(newlist.firstChild);
+    }
+} 
